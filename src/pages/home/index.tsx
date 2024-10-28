ab714936d50a182c2b5cb30265f15e272223433a
Roberto Melo <rvmelo@inovahu.com>
2024-10-28 20:43:30 -0300

    refactor: change folder structure

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,22 @@
+import { Card } from '@/components/card'
+import { CloseButton } from '@/components/closeButton'
+import { Input } from '@/components/input'
+import { LinkButton } from '@/components/linkButton'
+import { NavigationItem } from '@/components/navigationItem'
+import { SmallCard } from '@/components/smallCard'
+import { Tag } from '@/components/tag'
+import { ChartLineUp } from '@phosphor-icons/react'
+
+export default function Home() {
+  return (
+    <>
+      <LinkButton color="white" size="medium" />
+      <CloseButton onClick={() => undefined} />
+      <NavigationItem Icon={ChartLineUp} title="Início" />
+      <Input placeholder="Placeholder" />
+      <Tag name="Computação" />
+      <Card rate={3} />
+      <SmallCard rate={4} />
+    </>
+  )
+}
