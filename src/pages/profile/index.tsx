4f483b1e45844fac92f4017eac8494401cc24689
Roberto Melo <robertomelo7@hotmail.com>
2025-02-10 20:39:31 -0300

    feat: add getServerSideProps function on profile page

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { MenuGrid, ProfileContainer } from './styles'
 import { UserMenu } from '@/components/UserMenu'
+import { GetServerSideProps } from 'next'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '../api/auth/[...nextauth]'
+import { prisma } from '@/lib/prisma'
 
 export default function Profile() {
   return (
@@ -10,4 +14,45 @@ export default function Profile() {
       </MenuGrid>
     </ProfileContainer>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const session = await getServerSession(req, res, authOptions)
+
+  const { user } = session || {}
+
+  const foundUser = await prisma.user.findUnique({
+    where: {
+      id: user?.id,
+    },
+    select: {
+      name: true,
+      avatar_url: true,
+      created_at: true,
+      ratings: {
+        select: {
+          id: true,
+          rate: true,
+          book: {
+            select: {
+              name: true,
+              author: true,
+              summary: true,
+            },
+          },
+        },
+      },
+    },
+  })
+
+  return {
+    props: {
+      user: {
+        name: foundUser?.name,
+        avatar_url: foundUser?.avatar_url,
+        created_at: foundUser?.created_at,
+      },
+      ratings: foundUser?.ratings,
+    },
+  }
 }
